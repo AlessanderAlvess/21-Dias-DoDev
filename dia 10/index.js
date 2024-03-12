2459059e3bceae339570b0513ecc0305bbd3918a
Alessander Alves <alessander_jf@hotmail.com>
2024-03-12 19:53:11 -0300

    introdução a Arrays + exercicio

---
+++ dia 10/index.js
@@ -0,0 +1,23 @@
+// let nomes = [];
+// let continuar = true;
+// let posicaoDoArray = 0;
+// while(continuar){
+//     let nomeInserido = prompt('Insira um nome:');
+//     nomes[posicaoDoArray] = nomeInserido;
+
+//     let desejaContinuar = prompt('Insira 1 caso deseje adicionar um novo nome')   
+//     if(desejaContinuar != "1"){
+//         continuar = false;
+//     }     
+//     posicaoDoArray ++;
+// }
+
+
+
+let array = [0, 2, 3, 5, 9, 7, 1, 6, 8, 4];
+
+let numeroInserido = parseInt(prompt('Insira um numero inteiro e positivo'));
+
+for(numeroInserido = 0; numeroInserido < 10; numeroInserido++){
+    
+}
