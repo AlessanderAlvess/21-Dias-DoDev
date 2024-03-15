efdd8dbddc69e4ee189d613b4d1f3dabf67a09cf
Alessander Alves <alessander_jf@hotmail.com>
2024-03-15 13:29:24 -0300

    realizando exercicios de arrays

--- dia 10/index.js
+++ dia 10/index.js
@@ -12,12 +12,59 @@
 //     posicaoDoArray ++;
 // }
 
+///////////////////////////////////// EXERCICIO 1.1 /////////////////////////////////////
+
+// let array = [0, 2, 3, 5, 9, 7, 1, 6, 8, 4];
+
+// for(let i = 0; i < array.length; i++){
+//         const index = i;
+//         console.log(`O índice do elemento ${array[i]} é: ${index}`);
+// }
+
+///////////////////////////////////// EXERCICIO 1.2 /////////////////////////////////////
+
+//  let array = [];
+//  let arrayInvertido = [];
+
+// for(let i = 0; i < 5; i++){
+//         let numeroInserido = parseInt(prompt("Digite o " + (i + 1) + "º número"))
+//         array[i] = numeroInserido;
+// }
+
+// console.log('Este é o array original ' + array)
 
 
-let array = [0, 2, 3, 5, 9, 7, 1, 6, 8, 4];
+///////// ARRAY INVERTIDO ////////
 
-let numeroInserido = parseInt(prompt('Insira um numero inteiro e positivo'));
+// let j = 4;
+// for(let i = 0; i < 5; i++){
+//         arrayInvertido[i] = array[j]
+//         j--
+// }
 
-for(numeroInserido = 0; numeroInserido < 10; numeroInserido++){
-    
+// console.log('Este é o array invertido ' + arrayInvertido)
+
+
+///////////////////////////////////// EXERCICIO 1.3 /////////////////////////////////////
+// DECLARANDO AS VARIAVEIS
+
+let array = [];
+let arrayInvertido = [];
+let quantidade = parseInt(prompt('Digite a quantidade de números que queira inserir no array'))
+
+// PREENCHENDO OS ARRAYS
+
+for(let i = 0; i < quantidade; i++){
+        let numero = parseInt(prompt('Digite o' + (i + 1) + "º número"))
+        array[i] = numero;
 }
+        console.log('Este é o array original:' + array)
+
+// INVERTENDO O ARRAY
+let invertido = quantidade - 1;
+for(let i = 0; i < quantidade; i++){
+        arrayInvertido[i] = array[invertido]
+        invertido--
+}
+        console.log('Este é o array invertido:' + arrayInvertido)
+        
